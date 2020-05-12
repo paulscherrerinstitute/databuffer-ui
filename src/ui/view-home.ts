7a4fe8a8b51aaf11b9c208371bc8db91f65f6613
Daniel J. Lauk <daniel.lauk@psi.ch>
2020-05-12 15:22:56 +0200

    chore: redesign view-home
    Add app logo to home page.
    Add footer to bottom of page.
    Move version information to footer.
    Move link to changelog to footer.
    Add link to provide feedback to footer.

--- src/ui/view-home.ts
+++ src/ui/view-home.ts
@@ -44,29 +44,55 @@ export class HomeElement extends connect(store, LitElement) {
 
 	render() {
 		return html`
-			<div id="logo">DAQ Web UI</div>
-			<p id="version">
-				Version: APP_VERSION_STRING.
-				<a
-					href="https://github.com/paulscherrerinstitute/databuffer-ui/blob/master/CHANGELOG.md"
-					target="_blank"
-					>View changelog</a
-				>
-				for details.
-			</p>
+			<div id="top">
+				<div class="limit-width centered">
+					<div id="logo">
+						<img
+							src="/icons/android-icon-192x192.png"
+							alt="app logo"
+						/><br />Databuffer UI
+					</div>
 
-			<mwc-textfield
-				id="query"
-				label="Search for EPICS channels"
-				helper="Search supports regular expressions"
-				@keyup=${(e: KeyboardEvent): void => {
-					if (e.key === 'Enter') this.__search()
-				}}
-				value="${this.pattern}"
-			></mwc-textfield>
-			<mwc-button icon="search" raised @click=${this.__search}
-				>Search</mwc-button
-			>
+					<mwc-textfield
+						id="query"
+						label="Search for EPICS channels"
+						helper="Search supports regular expressions"
+						@keyup=${(e: KeyboardEvent): void => {
+							if (e.key === 'Enter') this.__search()
+						}}
+						value="${this.pattern}"
+					></mwc-textfield>
+					<mwc-button icon="search" raised @click=${this.__search}
+						>Search</mwc-button
+					>
+				</div>
+			</div>
+			<div id="bottom">
+				<div id="footer" class="limit-width">
+					<mwc-icon>info_outline</mwc-icon>
+					<span>
+						This is databuffer UI APP_VERSION_STRING.
+						<a
+							href="https://github.com/paulscherrerinstitute/databuffer-ui/blob/master/CHANGELOG.md"
+							target="_blank"
+							>View the changelog</a
+						>
+						for details about this version.
+					</span>
+					<mwc-icon>feedback</mwc-icon>
+					<span>
+						You can report issues or give feedback
+						<a
+							href="https://github.com/paulscherrerinstitute/databuffer-ui/issues/"
+							>on GitHub</a
+						>
+						or
+						<a href="mailto:daq@psi.ch?subject=Feedback%20on%20databuffer-ui"
+							>by email</a
+						>.
+					</span>
+				</div>
+			</div>
 		`
 	}
 
@@ -84,24 +110,52 @@ export class HomeElement extends connect(store, LitElement) {
 				:host {
 					display: flex;
 					flex-direction: column;
-					align-items: center;
-					margin: 0 auto;
+					height: 100%;
+				}
+
+				.limit-width {
 					max-width: 800px;
+					margin: 0 auto;
+				}
+
+				.centered {
+					text-align: center;
 				}
 
 				#logo {
-					font-size: 76pt;
+					font-size: 12px;
 					margin: 24px 0;
-				}
-
-				#version {
-					font-size: 8pt;
-					margin: 0;
 				}
 
 				#query {
 					margin: 24px 0;
 					width: 100%;
+				}
+
+				#top {
+					flex: 1;
+				}
+
+				#bottom {
+					flex: 0;
+					margin-top: 8rem;
+					padding: 16px 0;
+					font-size: 80%;
+					background-color: hsl(0, 0%, 30%);
+					color: hsl(0, 0%, 80%);
+				}
+
+				#bottom a,
+				#bottom a:visited {
+					color: var(--dui-secondary-light);
+				}
+
+				#footer {
+					display: grid;
+					grid-template-columns: auto 1fr;
+					grid-template-rows: auto auto auto;
+					gap: 8px;
+					align-items: center;
 				}
 			`,
 		]
