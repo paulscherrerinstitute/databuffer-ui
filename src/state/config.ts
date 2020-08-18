dbff1cc2fd95395bcf9874841f028f322d82f704
Daniel J. Lauk <daniel.lauk@psi.ch>
2020-08-18 08:09:40 +0200

    chore: add routing plugin to rdx

--- src/state/config.ts
+++ src/state/config.ts
@@ -1,4 +1,18 @@
+import createMatcher from '@captaincodeman/router'
+import { routingPlugin } from '@captaincodeman/rdx'
 import * as models from './models'
 
-// TODO: add routing plugin
-export const config = { models }
+const routes = {
+	'/': 'home',
+	'/search': 'channel-search',
+	'/preselect': 'preselect',
+	'/plot': `plot`,
+	'/plot/:backend/:name': `plot-single-channel`,
+	'/plot-settings': `plot-settings`,
+	'/query-meta': `query-meta`,
+	'/*': 'not-found',
+}
+const matcher = createMatcher(routes)
+const routing = routingPlugin(matcher)
+
+export const config = { models, plugins: { routing } }
