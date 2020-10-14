19eaba281eb57bda9a246d47fa5527e19c962bc8
Daniel J. Lauk <daniel.lauk@psi.ch>
2020-10-14 09:28:04 +0200

    build(ci): remove old build tools

--- docker/.dockerconfig.sh
+++
@@ -1,5 +0,0 @@
-# source this file for common docker config items
-
-DOCKERREGISTRY=${DOCKERREGISTRY:-docker.psi.ch:5000}
-DOCKERIMAGE=${DOCKERIMAGE:-databuffer-ui}
-DOCKERFULL=$DOCKERREGISTRY/$DOCKERIMAGE
