3ddd51d833ea0f1ac7111b50470eac6292a9fb6d
Daniel J. Lauk <daniel.lauk@psi.ch>
2020-11-11 11:31:20 +0100

    chore: add synchronized crosshair

--- src/ui/daq-plot/daq-plot-separate-plots.ts
+++ src/ui/daq-plot/daq-plot-separate-plots.ts
@@ -17,6 +17,93 @@ import {
 import { baseStyles } from '../shared-styles'
 import { DaqPlotYAxis, DaqPlotDataSeries } from './types'
 
+//#region Highcharts manipulations
+
+// add type declaration for the internal method `searchPoint`
+//
+// see: https://www.highcharts.com/forum/viewtopic.php?t=42923
+declare module 'highcharts' {
+	interface Series {
+		searchPoint: (
+			event: Highcharts.PointerEventObject,
+			flag: boolean
+		) => Highcharts.Point
+	}
+}
+
+// For synchronizing the charts' crosshairs we need to fix-up various
+// Highcharts prototypes.
+// (see https://www.highcharts.com/demo/synchronized-charts)
+//
+// **BUT**: As we use Highcharts in different parts of the app, we
+// can't just leave it at that. We have to undo it after removing this
+// type of element. Otherwise the change will affect charts in other
+// elements as well.
+let __orig_pointer_reset:
+	| typeof Highcharts.Pointer.prototype.reset
+	| null = null
+
+let __numConnectedElements = 0
+
+function __change_highcharts_prototypes() {
+	if (__orig_pointer_reset === null) {
+		__orig_pointer_reset = Highcharts.Pointer.prototype.reset
+		Highcharts.Pointer.prototype.reset = () => undefined
+	}
+}
+
+function __revert_highcharts_prototypes() {
+	if (__orig_pointer_reset === null) {
+		throw new Error('__orig_pointer_reset is null')
+	}
+	// if (__orig_point_highlight === null) {
+	// 	throw new Error('__orig_point_highlight is null')
+	// }
+	Highcharts.Pointer.prototype.reset = __orig_pointer_reset
+	__orig_pointer_reset = null
+	// Highcharts.Point.prototype.highlight = __orig_point_highlight
+	// __orig_point_highlight = null
+}
+
+function __registerElem() {
+	if (__numConnectedElements === 0) {
+		__change_highcharts_prototypes()
+	}
+	__numConnectedElements++
+}
+
+function __unregisterElem() {
+	__numConnectedElements--
+	if (__numConnectedElements === 0) {
+		__revert_highcharts_prototypes()
+	}
+}
+//#endregion
+
+/**
+ * Highlight a single point on the chart.
+ *
+ * This is taken and adapted from the Highcharts example at
+ * https://www.highcharts.com/demo/synchronized-charts
+ *
+ * The official example puts this as Highcharts.Point.prototype.highlight,
+ * but if we keep it off the prototype, it cannot clash with potential
+ * future developments of Highcharts.
+ *
+ * @param p The point to highlight
+ * @param event The MouseEvent that triggered the highlighting
+ */
+function highlightPoint(p: Highcharts.Point, event: MouseEvent) {
+	const ptrEvent = p.series.chart.pointer.normalize(event)
+	p.onMouseOver() // Show the hover marker
+	// NOTE: The example from the highcharts documentation has the following line
+	//       but it keeps raising an error somewhere in the internals.
+	//       The tooltip seems to be showing and refreshing just fine on its own.
+	//       So for now, let's just leave it commented out.
+	//this.series.chart.tooltip.refresh(this) // Show the tooltip
+	p.series.chart.xAxis[0].drawCrosshair(ptrEvent, p) // Show the crosshair
+}
+
 declare global {
 	interface HTMLElementTagNameMap {
 		'daq-plot-separate-plots': DaqPlotSeparatePlotsElement
@@ -43,6 +130,16 @@ export class DaqPlotSeparatePlotsElement extends LitElement {
 	private charts!: Highcharts.Chart[]
 
 	private previousCount: number = 0
+
+	connectedCallback() {
+		super.connectedCallback()
+		__registerElem()
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback()
+		__unregisterElem()
+	}
 
 	updated(changedProperties: PropertyValues) {
 		const opts: Highcharts.Options[] = []
@@ -105,8 +202,24 @@ export class DaqPlotSeparatePlotsElement extends LitElement {
 		}
 	}
 
+	// taken from the Highcharts demo and adpated
+	private chartMouseMove(e: MouseEvent) {
+		for (const chart of this.charts) {
+			// const chart = this.charts[i]
+			if (chart === undefined) continue
+			// Find coordinates within the chart
+			const event = chart.pointer.normalize(e)
+			// Get the hovered point
+			const point = chart.series[0].searchPoint(event, true)
+
+			if (point) {
+				highlightPoint(point, e)
+			}
+		}
+	}
+
 	render() {
-		return html`<div id="chartgroup"></div>`
+		return html`<div id="chartgroup" @mousemove=${this.chartMouseMove}></div>`
 	}
 
 	static get styles() {
