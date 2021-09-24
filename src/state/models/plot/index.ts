e3ee78d96391698c078b6ba03f00cbbd846b2186
Daniel J. Lauk <daniel.lauk@psi.ch>
2021-09-24 15:53:50 +0200

    refactor: separate plot types module

--- src/state/models/plot/index.ts
+++ src/state/models/plot/index.ts
@@ -37,70 +37,14 @@ import {
 	DataUiDataSeries,
 } from '../../../shared/dataseries'
 
-export enum PlotVariation {
-	/** 1 Y axis for all channels */
-	SingleAxis = 'single-axis',
-
-	/** 1 Y axis for each channel */
-	SeparateAxes = 'separate-axes',
-
-	/** 1 plot (with 1 Y axis) for each channel */
-	SeparatePlots = 'separate-plots',
-}
-export function isPlotVariation(val: any): val is PlotVariation {
-	if (typeof val !== 'string') return false
-	return Object.values(PlotVariation as Record<string, string>).includes(val)
-}
-
-/** type of YAxis */
-export type YAxisType = 'linear' | 'logarithmic'
-export function isYAxisType(val: any): val is YAxisType {
-	if (typeof val !== 'string') return false
-	return val === 'linear' || val === 'logarithmic'
-}
-
-/** YAxis is a value axis of the chart */
-export interface YAxis {
-	/** title defines the label next to the axis */
-	title: string
-	/** unit of values on the axis, e.g. 'mbar' */
-	unit: string
-	/** side defines where on the plot the axis should be put */
-	side: 'left' | 'right'
-	/** lowest allowed value. set to null for automatic minimum. */
-	min: number | null
-	/** highest value. set to null for automatic maximum. */
-	max: number | null
-	/** type of axis */
-	type: YAxisType
-}
-
-export type DownloadAggregation = 'as-is' | 'PT5S' | 'PT1M' | 'PT1H' | 'raw'
-
-export type PlotDataSeries = {
-	channel: DataUiChannel
-	label: string
-	yAxisIndex: number
-	fetching?: boolean
-	error?: Error
-	requestSentAt?: number
-	requestFinishedAt?: number
-	datapoints?: DataUiDataPoint<number, unknown>[]
-}
-
-export interface PlotState {
-	plotVariation: PlotVariation
-	plotTitle: string
-	startTime: number
-	endTime: number
-	dataSeries: PlotDataSeries[]
-	yAxes: YAxis[]
-	queryRangeShowing: boolean
-	dialogShareLinkShowing: boolean
-	dialogShareLinkAbsoluteTimes: boolean
-	dialogDownloadShowing: boolean
-	dialogDownloadAggregation: DownloadAggregation
-}
+import {
+	DownloadAggregation,
+	isPlotVariation,
+	isYAxisType,
+	PlotState,
+	PlotVariation,
+	YAxisType,
+} from './types'
 
 // helper functions
 const findIndexOfChannel = (state: PlotState, ch: DataUiChannel): number =>
