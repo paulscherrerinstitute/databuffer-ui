f15511d847a6b6e3226a75bdd695a5212f40adcc
Daniel J. Lauk <daniel.lauk@psi.ch>
2021-11-08 15:44:43 +0100

    chore: add factory for data api providers

--- src/api/queryrest.ts
+++ src/api/queryrest.ts
@@ -27,11 +27,47 @@ import {
 export const NR_OF_BINS = 500
 export type { DataResponse, ChannelNamesResponse }
 
-export class ApiV0QueryProvider {
+export interface DataApiProvider {
+	apiVersion: () => string
+	listBackends: () => Promise<string[]>
+	queryStringData: (
+		channel: DataUiChannel,
+		start: string,
+		end: string
+	) => Promise<DataUiDataSeries<number, string>>
+	queryData: (
+		channel: DataUiChannel,
+		start: string,
+		end: string
+	) => Promise<DataUiDataSeries<number, DataUiAggregatedValue>>
+	searchChannels: (nameRegex?: string) => Promise<DataUiChannel[]>
+}
+
+export const createDataApiProvider = async (
+	url: string
+): Promise<DataApiProvider> => {
+	// TODO: try to determine api version from known endpoints
+	// const resp = await fetch(`${url}/meta`)
+	// if (resp.ok) {
+	// 	const meta = await resp.json()
+	// 	if (meta.apiversion && meta.apiversion === '4') {
+	// 		return new ApiV4QueryProvider(url)
+	// 	}
+	//	throw new Error(`api version of ${url} not supported: ${meta.apiversion}`)
+	// }
+	// no enpoint found? fall back to APIv0 (which doesn't have such an endpoint)
+	return new ApiV0QueryProvider(url)
+}
+
+export class ApiV0QueryProvider implements DataApiProvider {
 	private api: QueryRest
 
 	constructor(private url: string) {
 		this.api = new QueryRest(url)
+	}
+
+	public apiVersion(): string {
+		return 'v0'
 	}
 
 	public listBackends(): Promise<string[]> {
