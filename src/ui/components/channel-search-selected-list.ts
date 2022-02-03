49208b35ec009291d68ec61d096d53edc48a98c4
Daniel J. Lauk <daniel.lauk@psi.ch>
2022-02-03 16:21:07 +0100

    refactor: simplify channel-search-selected-list

--- src/ui/components/channel-search-selected-list.ts
+++ src/ui/components/channel-search-selected-list.ts
@@ -1,17 +1,17 @@
-import { LitElement, css, html } from 'lit'
+import { LitElement, css, html, nothing } from 'lit'
 import { customElement, state } from 'lit/decorators.js'
+import { repeat } from 'lit/directives/repeat.js'
 import { connect } from '@captaincodeman/rdx'
 import pluralize from 'pluralize'
 
 import '@material/mwc-button'
-import '@material/mwc-icon-button'
-import 'weightless/list-item'
-
-import './channel-search-selected-item'
+import '@material/mwc-checkbox'
+import type { Checkbox } from '@material/mwc-checkbox'
 
 import { store, AppState } from '../../state/store'
 import { plotSelectors } from '../../state/models/plot'
-import { DataUiChannel } from '../../shared/channel'
+import { channelToId, DataUiChannel } from '../../shared/channel'
+import { mwcCheckboxPrimaryColor, textHelpers } from '../shared-styles'
 
 @customElement('channel-search-selected-list')
 export class ChannelSearchSelectedListElement extends connect(
@@ -33,6 +33,35 @@ export class ChannelSearchSelectedListElement extends connect(
 		}
 	}
 
+	private _renderItem(item: DataUiChannel, selectedIndex: number) {
+		return html`
+			<div>
+				<mwc-checkbox
+					?checked=${selectedIndex !== -1}
+					@change=${(e: Event) => {
+						const cb = e.target as Checkbox
+						if (cb.checked) {
+							store.dispatch.plot.selectChannel(item)
+						} else {
+							store.dispatch.plot.unselectChannel(selectedIndex)
+						}
+					}}
+				></mwc-checkbox>
+			</div>
+			<div>
+				${item.name}${item.description
+					? html`<br /><span class="opacity-70 text-smallest"
+								>${item.description}</span
+							>`
+					: nothing}
+			</div>
+			<div>${item.backend}</div>
+			<div>${item.dataType}</div>
+			<div>${item.dataType === 'string' ? 'string' : item.dataShape}</div>
+			<div>${item.unit}</div>
+		`
+	}
+
 	public render() {
 		return html`
 			<div id="toprow">
@@ -49,19 +78,24 @@ export class ChannelSearchSelectedListElement extends connect(
 					icon="show_chart"
 					raised
 					@click=${() => this.dispatchEvent(new CustomEvent('channel-plot'))}
-					.disabled=${this.selectedChannels.length === 0}
+					?disabled=${this.selectedChannels.length === 0}
 					>Plot selected</mwc-button
 				>
 			</div>
-			<div id="list">
-				${this.selectedChannels.map(
-					(ch, idx) =>
-						html`<channel-search-selected-item
-							.backend=${ch.backend}
-							.name=${ch.name}
-							@channel-search-selected-item:remove=${() =>
-								store.dispatch.plot.unselectChannel(idx)}
-						></channel-search-selected-item>`
+			<div id="list" class="fullwidth text-small">
+				<div class="list-header">
+					&nbsp;
+					<!-- spacer; need nonbreaking space to render at full height like regular text -->
+				</div>
+				<div class="list-header">Channel</div>
+				<div class="list-header">Backend</div>
+				<div class="list-header">Data type</div>
+				<div class="list-header">Data shape</div>
+				<div class="list-header">Unit</div>
+				${repeat(
+					this.selectedChannels,
+					ch => channelToId(ch),
+					(ch, idx) => this._renderItem(ch, idx)
 				)}
 			</div>
 		`
@@ -69,6 +103,8 @@ export class ChannelSearchSelectedListElement extends connect(
 
 	public static get styles() {
 		return [
+			textHelpers,
+			mwcCheckboxPrimaryColor,
 			css`
 				:host {
 					display: flex;
@@ -89,8 +125,26 @@ export class ChannelSearchSelectedListElement extends connect(
 				#list {
 					margin-top: 8px;
 					border: 1px solid rgba(0, 0, 0, 0.54);
-					flex: 1 1 auto;
 					overflow-y: scroll;
+					display: grid;
+					grid-template-columns: auto 1fr auto auto auto auto;
+					grid-template-rows: auto;
+					gap: 2px;
+					align-items: center;
+				}
+				#list > div {
+					padding-left: 4px;
+					padding-right: 4px;
+				}
+				.list-header {
+					text-align: center;
+					background-color: var(--dui-primary);
+					color: var(--dui-on-primary);
+					padding: 4px 8px;
+				}
+
+				.fullwidth {
+					width: 100%;
 				}
 			`,
 		]
